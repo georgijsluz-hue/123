2d27780f4c718a4ef9599e62571ea7716331be62
georgijsluz-hue <georgijsluz@gmail.com>
2026-03-23 20:41:54 +0300

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,11 @@
+print ("hello world")
+produkt1="sigaret"
+produkt2="сигарета"
+produkt3= "некурилы"
+gods=[produkt1 ,produkt2 ,produkt3]
+a=int(input("возраст"))
+if a<18:
+    print("нет")
+if a>=18:
+    print("добро пожаловать в табачку")
+    print(f"tovari {goods[0]}")
