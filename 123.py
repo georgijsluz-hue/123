e8865ec9d83b7e3d01cacef8445aa1e43d6b1352
georgijsluz-hue <georgijsluz@gmail.com>
2026-03-23 20:58:19 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,11 +1,12 @@
-print ("hello world")
-produkt1="sigaret"
-produkt2="сигарета"
-produkt3= "некурилы"
-gods=[produkt1 ,produkt2 ,produkt3]
+produkt1={"name":"1-sigaret", "price":200, "count":20}
+produkt2={"name":"2-сигарета", "price":100, "count":10}
+produkt3={"name":"3-некурилы", "price":50, "count":5}
+товары=(produkt1 ,produkt2 ,produkt3)
 a=int(input("возраст"))
 if a<18:
     print("нет")
 if a>=18:
     print("добро пожаловать в табачку")
-    print(f"tovari {goods[0]}")
+    print(f"tovari {товары}")
+    poisk=input("что хочешь?")
+    print(f"вот {товары[int(poisk)-1]}")
